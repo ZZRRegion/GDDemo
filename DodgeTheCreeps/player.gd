ec423784bfa7b20ec4caa119ab256c73ae1e2944
zzr <zhangzr@skeqi.com>
2025-07-05 09:25:26 +0800

    1

--- DodgeTheCreeps/player.gd
+++ DodgeTheCreeps/player.gd
@@ -6,47 +6,39 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed(&"ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed(&"ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed(&"ui_up"):
-		velocity.y -=1
-	if Input.is_action_pressed(&"ui_down"):
-		velocity.y += 1
-	
+	var velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
+		position += velocity * delta
+		position = position.clamp(Vector2.ZERO, screen_size)
+	
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		elif  velocity.y != 0:
+			$AnimatedSprite2D.animation = "up"
+			$AnimatedSprite2D.flip_v = velocity.y > 0
 	else:
 		$AnimatedSprite2D.stop()
 	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = &"right"
-		$AnimatedSprite2D.flip_v = false
-		$Trail.rotation = 0
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = &"up"
-		rotation = PI if velocity.y > 0 else 0
 	
 func start(pos:Vector2):
 	position = pos
-	rotation = 0
 	show()
 	$CollisionShape2D.disabled = false
 
 func _on_body_entered(_body):
 	hide()
 	hit.emit()
+	#敌人每次击中玩家时都会发出一个信号。要禁用玩家的碰撞检测，确保不会多次触发hit信号
+	#set_deferred可以安全的禁用形状
 	$CollisionShape2D.set_deferred(&"disabled", true)
 
 
