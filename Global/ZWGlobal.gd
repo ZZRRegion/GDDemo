98b1bec83f9f160905dce02d4ce1a00751dc01f7
zzr <zhangzr@skeqi.com>
2025-07-08 09:31:21 +0800

    12

--- Global/ZWGlobal.gd
+++ Global/ZWGlobal.gd
@@ -1,1 +1,29 @@
 extends Node
+@export var nameTxt : String = "zw"
+@export var age : int = 20
+var file_path :String = "user://demo.json"
+func to_dict() -> Dictionary:
+	return {
+		"nameTxt": nameTxt,
+		"age" : age,
+		"file_path": file_path,
+		"last_time" : Time.get_datetime_string_from_system()
+	}
+func _ready() -> void:
+	load_data()
+func _notification(what: int) -> void:
+	if what == Node.NOTIFICATION_WM_CLOSE_REQUEST:
+		save()
+		
+func load_data() -> void:
+	if !FileAccess.file_exists(file_path):
+		return
+	var content := FileAccess.get_file_as_string(file_path)
+	print(content)
+	
+func save() -> void:
+	var file := FileAccess.open(file_path, FileAccess.WRITE)
+	var dict = to_dict()
+	var content := JSON.stringify(dict)
+	file.store_string(content)
+	file.close()
--- Planet/main.gd
+++ Planet/main.gd
@@ -1,22 +1,12 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var maxspeed : float = 200
-@export var accel : float = 150
-@export var fricition : float = 75
 
-var input : Vector2 = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(ZwGlobal.nameTxt)
+	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
-	movement(delta)
-	move_and_slide()
-	
-func movement(delta : float) -> void:
-	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	if input.length() == 0:
-		if velocity.length() > fricition * delta:
-			velocity -= velocity.normalized()
-		else:
-			velocity = Vector2.ZERO
-	else:
-		velocity += input * accel * delta
-		velocity = velocity.limit_length(maxspeed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
