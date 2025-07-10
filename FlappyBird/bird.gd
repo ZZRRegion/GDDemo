f8bdbb0a9f2cb179f3ef0087332df0b85b3d63b6
zzr <zhangzr@skeqi.com>
2025-07-10 09:29:13 +0800

    3

--- FlappyBird/bird.gd
+++ FlappyBird/bird.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const JUMP_VELOCITY = -400.0
-
+var rot_degree :float = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,5 +11,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = JUMP_VELOCITY
-
+	
+	rot_degree = clampf(-30 * velocity.y / JUMP_VELOCITY, -30, 30)
+	rotation_degrees = rot_degree
 	move_and_slide()
