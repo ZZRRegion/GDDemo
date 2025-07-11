5b3a30ff53599272d2aabb3ab2c07d5a713cfba2
zzr <zhangzr@skeqi.com>
2025-07-11 15:37:39 +0800

    1

--- Topdown/Scenes/Player/player.gd
+++ Topdown/Scenes/Player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
-
+class_name Player
+@export var move_speed: int = 100
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_direction * move_speed
+	handle_animation(move_direction)
+	handle_rotation(move_direction)
+	move_and_slide()
+
+func handle_animation(move_direction: Vector2) -> void:
+	if move_direction.length() > 0:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+
+func handle_rotation(move_direction: Vector2) -> void:
+	if move_direction.x > 0:
+		animated_sprite_2d.scale = Vector2(1, 1)
+	elif move_direction.x < 0:
+		animated_sprite_2d.scale = Vector2(-1, 1)
