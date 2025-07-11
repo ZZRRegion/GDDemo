35aee10000687c4ca50ec3b7b828f6696bdb1be8
zzr <zhangzr@skeqi.com>
2025-07-11 11:48:06 +0800

    1

--- PoderSolar/scenes/gamemanager.gd
+++ PoderSolar/scenes/gamemanager.gd
@@ -1,9 +1,15 @@
 extends Node
-
+const MONEY_DEFAULT: int = 300
+const ENERGY_DEFAULT: int = 25
+const POLLUTION_DEFAULT: int = 0
+const MAX_ENERGY: int = 50
+const MAX_POLLUTION: int = 50
+const CURRENT_LEVEL_DEFAULT: int = 1
 signal start_level
 signal money_change
 signal pollution_change
 signal  energy_change
+# 放置区域限定
 var appoint_rect: Rect2
 var end_scene: PackedScene = load("res://PoderSolar/scenes/menu/end.tscn")
 enum End_States {
@@ -14,12 +20,10 @@ enum End_States {
 }
 
 var end_state: End_States
-var current_level: int = 0
-var money: int = 300
-var max_energy: int = 50
-var max_pollution: int = 50
-var energy: int = 25
-var pollution: int = 0
+var current_level: int = CURRENT_LEVEL_DEFAULT
+var money: int = MONEY_DEFAULT
+var energy: int = ENERGY_DEFAULT
+var pollution: int = POLLUTION_DEFAULT
 
 func run_game() -> void:
 	Engine.time_scale = 1
@@ -28,10 +32,10 @@ func pause_game() -> void:
 	Engine.time_scale = 0
 
 func reset() -> void:
-	current_level = 0
-	money = 300
-	energy = 25
-	pollution = 0
+	current_level = CURRENT_LEVEL_DEFAULT
+	money = MONEY_DEFAULT
+	energy = ENERGY_DEFAULT
+	pollution = POLLUTION_DEFAULT
 
 func add_money(quantity: int) -> void:
 	money += quantity
@@ -39,17 +43,17 @@ func add_money(quantity: int) -> void:
 	
 func add_pollution(quantity: int) -> void:
 	pollution += quantity
-	if pollution > max_pollution:
+	if pollution > MAX_POLLUTION:
 		load_end_screen(End_States.LOOSE_POLLUTION)
 	elif pollution < 0:
-		pollution = 0
+		pollution = POLLUTION_DEFAULT
 	
 	pollution_change.emit()
 	
 func add_energy(quantity: int) -> void:
 	energy += quantity
 	energy_change.emit()
-	if energy > max_energy:
+	if energy > MAX_ENERGY:
 		load_end_screen(End_States.LOOSE_ENERGY_OVERCHARGE)
 	elif energy < 0:
 		load_end_screen(End_States.LOOSE_ENGERGY_OUT)
