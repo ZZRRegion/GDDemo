93b64c27643d4dad00b34db8bbae4f1920ea8ad1
zzr <zhangzr@skeqi.com>
2025-07-11 17:20:25 +0800

    12

--- Topdown/Scenes/Player/shoot_attack.gd
+++ Topdown/Scenes/Player/shoot_attack.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 func on_time_out() -> void:
 	ready_to_shoot = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	handle_shoot()
 	
 func handle_shoot() -> void:
@@ -25,11 +25,11 @@ func handle_shoot() -> void:
 	
 	if not ready_to_shoot:
 		return
-	var projectile:Projectile = projectile.instantiate() as Projectile
-	get_tree().current_scene.add_child(projectile)
+	var _projectile:Projectile = projectile.instantiate() as Projectile
+	get_tree().current_scene.add_child(_projectile)
 	var shoot_direction = get_global_mouse_position() - global_position
-	projectile.set_up(shoot_direction)
-	projectile.global_position = global_position
+	_projectile.set_up(shoot_direction)
+	_projectile.global_position = global_position
 	
 	shoot_t_imer.start()
 	ready_to_shoot = false
