a478962635e46506ffd77e4bfb4435deb11f4d14
zzr <zhangzr@skeqi.com>
2025-07-16 19:31:40 +0800

    时钟修改

--- RGTime/main.gd
+++ RGTime/main.gd
@@ -1,14 +1,21 @@
 extends Control
-
+@onready var cur_time: Label = $MarginContainer/VBoxContainer/CurTime
+const scale_num: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_window().title = "时间"
-	pass # Replace with function body.
-	
-
+	var win := get_window()
+	win.title = "时间"
+	win.size /= scale_num
+	self.scale = Vector2(scale_num, scale_num)
+	var screen_size := DisplayServer.screen_get_size()
+	win.position = (screen_size - win.size) / 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var t = Time.get_time_string_from_system()
-	$CurTime.text = t
+	cur_time.text = Time.get_time_string_from_system()
 	pass
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	get_window().always_on_top = toggled_on
+	pass # Replace with function body.
--- main.gd
+++ main.gd
@@ -1,26 +1,11 @@
 extends Node
-@onready var enery: CharacterBody2D = $Enery
-@onready var player: Area2D = $Player
-const SPEED: int = 300
+@onready var color_rect: ColorRect = $ColorRect
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(ResourceUID.create_id())
+	print(ResourceUID.create_id())
+	print(ResourceUID.id_to_text(22255555))
+
+func _on_button_pressed() -> void:
+	get_tree().quit(11)
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
-		enery.position += Vector2.LEFT * delta * SPEED
-	elif Input.is_action_pressed("move_right"):
-		enery.position += Vector2.RIGHT * delta * SPEED
-		
-	if Input.is_action_pressed("move_up"):
-		player.position += Vector2.LEFT * delta * SPEED
-	elif Input.is_action_pressed("move_down"):
-		player.position += Vector2.RIGHT * delta * SPEED
-	pass
-
-
-func _on_player_body_entered(body: Node2D) -> void:
-	print("player->" + str(body))
